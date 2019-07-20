43b846319990a0199fce2c7d6aae82bb76527858
Sridharan Soundara Rajan <ss051010@github.cerner.com>
2019-07-20 08:20:58 +0530

    videos-YT

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'your api key here';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+})
